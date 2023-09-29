a967d38b117bad8d3469f92c76a48a9caa14bee8
vitaxa <vitaxa93gamebox@gmail.com>
2023-09-29 12:02:51 +0300

    fix simple price calculator

--- src/main/kotlin/dev/crashteam/keanalytics/calculator/SimplePriceCalculator.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/calculator/SimplePriceCalculator.kt
@@ -1,5 +1,7 @@
 package dev.crashteam.keanalytics.calculator
 
+import dev.crashteam.keanalytics.domain.mongo.AdvancedSubscription
+import dev.crashteam.keanalytics.domain.mongo.ProSubscription
 import java.math.BigDecimal
 
 class SimplePriceCalculator(
@@ -7,6 +9,31 @@ class SimplePriceCalculator(
 ) : PriceCalculator {
 
     override suspend fun calculatePrice(): BigDecimal {
-        return priceCalculatorOption.context.subscription.price().toBigDecimal()
+        val priceCalculatorContext = priceCalculatorOption.context
+        val subscriptionPrice = priceCalculatorContext.subscription.price()
+        if (priceCalculatorContext.multiply > 1) {
+            val multipliedAmount =
+                BigDecimal(subscriptionPrice) * BigDecimal.valueOf(priceCalculatorContext.multiply.toLong())
+            val discount = if (priceCalculatorContext.multiply == 3.toShort()) {
+                when (priceCalculatorContext.subscription) {
+                    AdvancedSubscription -> {
+                        BigDecimal(0.15)
+                    }
+
+                    ProSubscription -> {
+                        BigDecimal(0.20)
+                    }
+
+                    else -> BigDecimal(0.10)
+                }
+            } else if (priceCalculatorContext.multiply >= 6) {
+                BigDecimal(0.30)
+            } else BigDecimal(0.10)
+            (multipliedAmount - (multipliedAmount * discount)).toLong().toString()
+
+            return priceCalculatorOption.context.subscription.price().toBigDecimal()
+        } else {
+            return priceCalculatorOption.context.subscription.price().toBigDecimal()
+        }
     }
 }
