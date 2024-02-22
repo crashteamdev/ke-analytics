d83b1ef24cbe25d40667bddd53a56209ccaf5324
vitaxa <vitaxa93gamebox@gmail.com>
2024-02-22 12:53:36 +0300

    add user subscription giveaway [3]

--- src/main/kotlin/dev/crashteam/keanalytics/service/UserSubscriptionService.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/service/UserSubscriptionService.kt
@@ -1,10 +1,11 @@
 package dev.crashteam.keanalytics.service
 
 import dev.crashteam.keanalytics.domain.mongo.SubscriptionDocument
+import dev.crashteam.keanalytics.domain.mongo.UserDocument
 import dev.crashteam.keanalytics.repository.mongo.UserRepository
 import dev.crashteam.keanalytics.service.exception.UserSubscriptionGiveawayException
-import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 import java.time.LocalDate
 
 @Service
@@ -12,20 +13,23 @@ class UserSubscriptionService(
     private val userRepository: UserRepository,
 ) {
 
-    suspend fun giveawayDemoSubscription(userId: String) {
-        val userDocument = userRepository.findByUserId(userId).awaitSingleOrNull()
-            ?: throw UserSubscriptionGiveawayException("User $userId not found")
-        if (userDocument.subscription != null) {
-            throw UserSubscriptionGiveawayException("User $userId already had subscription")
-        }
-        val updateUserDocument = userDocument.copy(
-            subscription = SubscriptionDocument(
-                subType = "demo",
-                createdAt = LocalDate.now().atStartOfDay(),
-                endAt = LocalDate.now().atStartOfDay().plusDays(5)
+    fun giveawayDemoSubscription(userId: String): Mono<UserDocument> {
+        return userRepository.findByUserId(userId).flatMap { userDocument ->
+            if (userDocument == null) {
+                return@flatMap Mono.error(UserSubscriptionGiveawayException("User $userId not found"))
+            }
+            if (userDocument.subscription != null) {
+                return@flatMap Mono.error(UserSubscriptionGiveawayException("User $userId already had subscription"))
+            }
+            val updateUserDocument = userDocument.copy(
+                subscription = SubscriptionDocument(
+                    subType = "demo",
+                    createdAt = LocalDate.now().atStartOfDay(),
+                    endAt = LocalDate.now().atStartOfDay().plusDays(5)
+                )
             )
-        )
-        userRepository.save(updateUserDocument).awaitSingleOrNull()
+            userRepository.save(updateUserDocument)
+        }
     }
 
 }
