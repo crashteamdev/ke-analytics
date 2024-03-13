4a2efafe3711b718efe2fe6a6c04c8c8f6a3c606
vitaxa <vitaxa93gamebox@gmail.com>
2024-03-13 19:47:21 +0300

    fix zero value when percentage calculation

--- src/main/kotlin/dev/crashteam/keanalytics/math/MathUtils.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/math/MathUtils.kt
@@ -16,6 +16,8 @@ object MathUtils {
     fun percentageDifference(a: Long, b: Long): Double {
         return if (a == 0L && b == 0L) {
             0.0
+        } else if (a == 0L) {
+            0.0
         } else {
             val difference = b - a
             (difference.toDouble() / a) * 100
