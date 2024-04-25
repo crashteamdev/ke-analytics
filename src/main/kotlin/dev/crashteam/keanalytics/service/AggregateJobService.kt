5096d8d0ae678708a1ba551a7c6c9be62a44b368
vitaxa <vitaxa93gamebox@gmail.com>
2024-04-25 19:58:26 +0300

    fix redis bean injection

--- src/main/kotlin/dev/crashteam/keanalytics/service/AggregateJobService.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/service/AggregateJobService.kt
@@ -1,33 +1,38 @@
 package dev.crashteam.keanalytics.service
 
 import dev.crashteam.keanalytics.service.model.StatType
-import org.springframework.data.redis.core.RedisTemplate
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.runBlocking
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.stereotype.Service
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneOffset
-import java.util.concurrent.TimeUnit
 
 @Service
 class AggregateJobService(
-    private val reactiveRedisTemplate: RedisTemplate<String, Long>
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, Long>
 ) {
 
-    fun checkCategoryAlreadyAggregated(tableName: String, categoryId: Long, statType: StatType): Boolean {
+    fun checkCategoryAlreadyAggregated(
+        tableName: String,
+        categoryId: Long,
+        statType: StatType
+    ): Boolean = runBlocking {
         val key = toKey(tableName, categoryId, statType)
-        val keyValue = reactiveRedisTemplate.opsForValue().get(key)
-        return keyValue != null && keyValue >= 1
+        val keyValue = reactiveRedisTemplate.opsForValue().get(key).awaitSingleOrNull()
+        keyValue != null && keyValue >= 1
     }
 
-    fun putCategoryAggregate(tableName: String, categoryId: Long, statType: StatType) {
+    fun putCategoryAggregate(tableName: String, categoryId: Long, statType: StatType) = runBlocking {
         val key = toKey(tableName, categoryId, statType)
-        reactiveRedisTemplate.opsForValue().set(key, 1)
+        reactiveRedisTemplate.opsForValue().set(key, 1).awaitSingleOrNull()
 
         val now = LocalDateTime.now().atOffset(ZoneOffset.UTC)
         val midnight = now.plusDays(1).with(LocalTime.MIDNIGHT)
         val ttlSeconds = Duration.between(now, midnight).seconds
-        reactiveRedisTemplate.expire(key, ttlSeconds, TimeUnit.SECONDS)
+        reactiveRedisTemplate.expire(key, Duration.ofSeconds(ttlSeconds)).awaitSingleOrNull()
     }
 
     private fun toKey(tableName: String, categoryId: Long, statType: StatType) =
