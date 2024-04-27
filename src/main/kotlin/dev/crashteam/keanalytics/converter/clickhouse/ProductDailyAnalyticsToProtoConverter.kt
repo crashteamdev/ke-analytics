41091ea0172f41e6646c20bbb28884ab6e0c7839
vitaxa <vitaxa93gamebox@gmail.com>
2024-04-27 13:57:48 +0300

    fix empty full price on product analytics

--- src/main/kotlin/dev/crashteam/keanalytics/converter/clickhouse/ProductDailyAnalyticsToProtoConverter.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/converter/clickhouse/ProductDailyAnalyticsToProtoConverter.kt
@@ -25,7 +25,7 @@ class ProductDailyAnalyticsToProtoConverter :
                 this.sellerTitle = source.seller.sellerTitle
             }.build()
             this.price = source.price.toMoney()
-            this.fullPrice = source.fullPrice?.toMoney()
+            source.fullPrice?.let { this.fullPrice = source.fullPrice.toMoney() }
             this.revenue = source.revenue.toMoney()
             this.appearAt = source.firstDiscovered.toLocalDate().toProtobufDate()
             this.reviewsCount = source.reviewAmount
