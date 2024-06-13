8e6297aa9d5df24a8a99f7c13d5db9f3a842fa2a
vitaxa <vitaxa93gamebox@gmail.com>
2024-06-13 10:30:24 +0300

    improve category analytics cache (#12)

--- src/main/kotlin/dev/crashteam/keanalytics/rpc/ExternalCategoryAnalyticsService.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/rpc/ExternalCategoryAnalyticsService.kt
@@ -6,7 +6,7 @@ import dev.crashteam.keanalytics.extensions.toRepositoryDomain
 import dev.crashteam.keanalytics.repository.clickhouse.model.SortBy
 import dev.crashteam.keanalytics.repository.clickhouse.model.SortField
 import dev.crashteam.keanalytics.repository.mongo.UserRepository
-import dev.crashteam.keanalytics.service.CategoryAnalyticsCacheableDecorator
+import dev.crashteam.keanalytics.service.CategoryAnalyticsSortableDecorator
 import dev.crashteam.keanalytics.service.ProductServiceAnalytics
 import dev.crashteam.keanalytics.service.UserRestrictionService
 import dev.crashteam.mp.base.DatePeriod
@@ -23,7 +23,7 @@ private val log = KotlinLogging.logger {}
 
 @GrpcService
 class ExternalCategoryAnalyticsService(
-    private val categoryAnalyticsService: CategoryAnalyticsCacheableDecorator,
+    private val categoryAnalyticsService: CategoryAnalyticsSortableDecorator,
     private val productServiceAnalytics: ProductServiceAnalytics,
     private val conversionService: ConversionService,
     private val userRepository: UserRepository,
@@ -80,8 +80,8 @@ class ExternalCategoryAnalyticsService(
                     log.error(e) { "Exception during get categories. request=$request" }
                     throw e
                 }
-            }.categoryAnalytics
-            if (categoriesAnalytics.isNullOrEmpty()) {
+            }
+            if (categoriesAnalytics.isEmpty()) {
                 log.debug { "Failed get category analytics. Empty categoryAnalytics response" }
                 responseObserver.onNext(GetCategoryAnalyticsResponse.newBuilder().apply {
                     this.errorResponse = GetCategoryAnalyticsResponse.ErrorResponse.newBuilder().apply {
--- src/main/kotlin/dev/crashteam/keanalytics/service/CategoryAnalyticsSortableDecorator.kt
+++ src/main/kotlin/dev/crashteam/keanalytics/service/CategoryAnalyticsSortableDecorator.kt
@@ -1,52 +1,47 @@
 package dev.crashteam.keanalytics.service
 
-import dev.crashteam.keanalytics.config.RedisConfig
 import dev.crashteam.keanalytics.repository.clickhouse.model.ChCategoryHierarchy
 import dev.crashteam.keanalytics.repository.clickhouse.model.SortBy
 import dev.crashteam.keanalytics.service.model.CategoryAnalyticsCacheableWrapper
+import dev.crashteam.keanalytics.service.model.CategoryAnalyticsInfo
 import dev.crashteam.keanalytics.service.model.CategoryDailyAnalytics
 import dev.crashteam.mp.base.DatePeriod
 import dev.crashteam.mp.base.Filter
 import dev.crashteam.mp.base.LimitOffsetPagination
 import dev.crashteam.mp.base.Sort
 import dev.crashteam.mp.external.analytics.category.ProductAnalytics
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
-class CategoryAnalyticsCacheableDecorator(
+class CategoryAnalyticsSortableDecorator(
     private val categoryAnalyticsService: CategoryAnalyticsService
 ) {
 
-    @Cacheable(
-        value = [RedisConfig.EXTERNAL_CATEGORY_ANALYTICS_CACHE_NAME],
-        key = "{#datePeriod, #sortBy}",
-        unless = "#result == null || #result.categoryAnalytics.isEmpty()"
-    )
     suspend fun getRootCategoryAnalytics(
         datePeriod: DatePeriod, sortBy: SortBy? = null
-    ): CategoryAnalyticsCacheableWrapper {
-        return CategoryAnalyticsCacheableWrapper(
-            categoryAnalyticsService.getRootCategoryAnalytics(
-                datePeriod, sortBy
-            )
-        )
+    ): List<CategoryAnalyticsInfo> {
+        var categoryAnalytics = categoryAnalyticsService.getRootCategoryAnalytics(datePeriod).categoryAnalytics
+            ?: return emptyList()
+        categoryAnalytics = if (sortBy != null) {
+            categoryAnalyticsService.sortCategoryAnalytics(categoryAnalytics, sortBy)
+        } else {
+            categoryAnalytics
+        }
+        return categoryAnalytics
     }
 
-    @Cacheable(
-        value = [RedisConfig.EXTERNAL_CATEGORY_ANALYTICS_CACHE_NAME],
-        key = "{#categoryId, #datePeriod, #sortBy}",
-        unless = "#result == null || #result.categoryAnalytics.isEmpty()"
-    )
     suspend fun getCategoryAnalytics(
         categoryId: Long, datePeriod: DatePeriod, sortBy: SortBy? = null
-    ): CategoryAnalyticsCacheableWrapper {
-        return CategoryAnalyticsCacheableWrapper(
-            categoryAnalyticsService.getCategoryAnalytics(
-                categoryId, datePeriod, sortBy
-            )
-        )
+    ): List<CategoryAnalyticsInfo> {
+        var categoryAnalytics = categoryAnalyticsService.getCategoryAnalytics(categoryId, datePeriod).categoryAnalytics
+            ?: return emptyList()
+        categoryAnalytics = if (sortBy != null) {
+            categoryAnalyticsService.sortCategoryAnalytics(categoryAnalytics, sortBy)
+        } else {
+            categoryAnalytics
+        }
+        return categoryAnalytics
     }
 
     fun getCategoryDailyAnalytics(
